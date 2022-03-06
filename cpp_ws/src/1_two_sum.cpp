ff793e42a2e595dee3e13714776c9d90415fde78
yuezhezhang <zyz994141@gmail.com>
2022-03-06 11:57:08 +0100

    Simplify the iteration and make the info function readable

--- cpp_ws/src/1_two_sum.cpp
+++ cpp_ws/src/1_two_sum.cpp
@@ -5,17 +5,20 @@
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        // get the lengthe of the vector
         int length = nums.size();
         if (length <= 1) throw "The input does not meet the requirement";
 
+        // instance the result vector
         std::vector<int> result_index;
         int flag = 0;
 
-        for (auto i=0; i<length; ++i)
+        // start iteration
+        for (auto i = 0; i < length; ++i)
         {
-            for (auto j = 0; j<length; ++j)
+            for (auto j = i+1; j < length; ++j)
             {
-                if ((i < j) && (nums[i]+nums[j] == target) )
+                if (nums[i] + nums[j] == target)
                 {   
                     flag = 1;
                     result_index.push_back(i);
@@ -23,21 +26,35 @@ public:
                 }
             } 
         }
+
+        // check if the solution found
         if (flag == 0)
             std::cout << "There is no soution" << std::endl; 
+        
         return result_index;
     }
 
     void info(std::vector<int> result_index)
-    {
-        for (auto i=0; i<result_index.size(); ++i)
-            std::cout << result_index[i] << std::endl;
+    {   
+        int length = result_index.size();
+        if (length > 1)
+        {
+            std::cout << "The vector is: [";
+            for (auto i = 0; i < length - 1; ++i)
+                std::cout << result_index[i] << ", ";
+            std::cout << result_index[length-1] << "]" << std::endl;
+        }
     }
 };
 
 
 int main(){
     Solution solution;
+
+    std::vector<int> nums0 = {2, 7, 11, 15};
+    int target0 = 7;
+    std::vector<int> result_index0 = solution.twoSum(nums0, target0);
+    solution.info(result_index0);
 
     std::vector<int> nums1 = {2, 7, 11, 15};
     int target1 = 9;
@@ -53,7 +70,4 @@ int main(){
     int target3 = 6;
     std::vector<int> result_index3 = solution.twoSum(nums3, target3);
     solution.info(result_index3);
-
-
-
 }
