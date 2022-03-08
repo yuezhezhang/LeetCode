592a3c800471d9834f16493211537584b390a90d
yuezhezhang <zyz994141@gmail.com>
2022-03-08 23:00:04 +0100

    Another approach using map is not working

--- cpp_ws/src/242_valid_anagram.cpp
+++ cpp_ws/src/242_valid_anagram.cpp
@@ -44,6 +44,52 @@ public:
 };
 
 
+class Solution2 {
+public:
+    bool isAnagram(std::string s, std::string t) {
+        std::vector<char> vector_s(s.begin(), s.end()); 
+        std::vector<char> vector_t(t.begin(), t.end());
+        // info(vector_s);
+
+        if (vector_s.size() != vector_t.size())
+        {
+            return false;
+        }
+
+        // this approach is not working 
+        std::unordered_map<char, int> map_s;
+        std::unordered_map<char, int> map_t;
+
+        for (auto i = 0; i < vector_s.size(); ++i)
+        {
+            map_s.insert({vector_s[i], i});
+            map_t.insert({vector_t[i], i});
+        }
+        for (auto it=map_t.begin(); it!=map_t.end(); ++it)
+            std::cout << it->first << " => " << it->second << '\n';
+        for (auto i = 0; i < vector_s.size(); ++i)
+        {
+            // there may exist conflicting
+            auto iter = map_t.find(vector_s[i]);
+            std::cout << i << " " ;
+            if (iter == map_t.end())
+            {
+                return false;
+            }
+            else
+            {   
+                std::cout << iter->first << std::endl;
+                map_t.erase(iter);
+                for (auto it=map_t.begin(); it!=map_t.end(); ++it)
+                    std::cout << it->first << " => " << it->second << '\n';
+            }
+        }
+        
+        return true;
+    }
+};
+
+
 int main(){
     Solution solution;
 
