0c607d0a0d4e1d233f231fadc0839c052b4f3dab
yuezhezhang <zyz994141@gmail.com>
2022-03-08 20:58:51 +0100

    Implement the anagram in python use sorted list

--- python_ws/242_valid_anagram.py
+++ python_ws/242_valid_anagram.py
@@ -5,3 +5,22 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # list.sort() returns the list in place, it does not return a new list
+        # answer = sorted(list) will return a new list
+        list_s = list(s)
+        list_s.sort()
+        list_t = list(t)  
+        list_t.sort()
+        return list_s == list_t 
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    s1 = "anagram"
+    t1 = "nagaram"
+    print(solution.isAnagram(s1, t1))
+
+    s2 = "rat"
+    t2 = "car"
+    print(solution.isAnagram(s2, t2))
