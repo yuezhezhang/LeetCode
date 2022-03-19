80968ca499d33ffbae3e3fe325168946af0ce319
yuezhezhang <zyz994141@gmail.com>
2022-03-19 23:19:59 +0100

    Implement the two solutions in python

--- python_ws/0349_intersection_of_two_arrays.py
+++ python_ws/0349_intersection_of_two_arrays.py
@@ -5,3 +5,18 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        # intersection of sets: &
+        # union of sets: |
+        return list(set(nums1) & set (nums2))
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    nums1 = [1,2,2,1]
+    nums2 = [2,2]
+    print(solution.intersection(nums1, nums2))
+
+    nums1 = [4,9,5]
+    nums2 = [9,4,9,8,4]
+    print(solution.intersection(nums1, nums2))
--- python_ws/1002_find_common_characters.py
+++ python_ws/1002_find_common_characters.py
@@ -4,3 +4,28 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
+        # get the intersection characters
+        intersection_set = set(words[0])
+        for word in words:
+            intersection_set = intersection_set & set(word)
+        common_chars = []
+        for char in intersection_set:
+            num = 100
+            for word in words:
+                if list(word).count(char) < num:
+                    num = list(word).count(char)
+            for i in range(num):
+                common_chars.append(char)
+        
+        return common_chars
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    words_1 = ["bella","label","roller"]
+    print(solution.commonChars(words_1))
+
+    words_2 = ["cool","lock","cook"]
+    print(solution.commonChars(words_2))
