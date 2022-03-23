fd92807db3a9af0bfb9d4cda6086394128643196
yuezhezhang <zyz994141@gmail.com>
2022-03-23 22:22:51 +0100

    Use an efficient function to get the intersection of two arrays

--- cpp_ws/src/0349_intersection_of_two_arrays.cpp
+++ cpp_ws/src/0349_intersection_of_two_arrays.cpp
@@ -34,7 +34,7 @@ public:
 class Solution2 {
 public:
     std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
-        // initialize the result vector
+        // declare the result vector
         std::vector<int> result;
         for (auto i = 0; i < nums1.size(); ++i) {
             auto iter = std::find(nums2.begin(), nums2.end(), nums1[i]);
@@ -52,9 +52,30 @@ public:
     }
 };
 
+// time: 4ms, beat 88.58%
+// RAM: 9.6MB, beat 95.61%
+class Solution3 {
+public:
+    std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
+        // sort and get the unique values 
+        // ref: https://stackoverflow.com/questions/1041620/whats-the-most-efficient-way-to-erase-duplicates-and-sort-a-vector
+        std::sort(nums1.begin(), nums1.end());
+        nums1.erase(std::unique(nums1.begin(), nums1.end()), nums1.end());
+        std::sort(nums2.begin(), nums2.end());
+        nums2.erase(std::unique(nums2.begin(), nums2.end()), nums2.end());
+        // declare the result vector
+        std::vector<int> result;
+        // use the library ref: https://en.cppreference.com/w/cpp/algorithm/set_intersection
+        std::set_intersection(nums1.begin(), nums1.end(),
+                              nums2.begin(), nums2.end(),
+                              std::back_inserter(result));
+        return result;
+    }
+};
+
 
 int main(){
-    Solution2 solution;
+    Solution3 solution;
 
     std::vector<int> nums1 = {1, 2, 2, 1};
     std::vector<int> nums2 = {2, 2};
