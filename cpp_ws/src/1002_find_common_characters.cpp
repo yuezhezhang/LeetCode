d90eef0014dd1759dafe0ba229cd880d42ca7887
yuezhezhang <zyz994141@gmail.com>
2022-03-24 01:14:14 +0100

    Solution 2 is not working because it cannot find the frequency of the characters

--- cpp_ws/src/1002_find_common_characters.cpp
+++ cpp_ws/src/1002_find_common_characters.cpp
@@ -51,8 +51,84 @@ public:
     }
 };
 
+class Test {
+public:
+    std::vector<char> commonChars(std::vector<std::string>& words) {
+        std::vector<char> result;
+        std::cout << words[0] << std::endl;
+        // std::cout << words[0].begin() << std::endl;
+        std::vector<char> word0(words[0].begin(), words[0].end()); // not working for string
+        std::vector<char> word1(words[1].begin(), words[1].end());
+
+        std::set_intersection(word0.begin(), word0.end(),
+                              word1.begin(), word1.end(),
+                              std::back_inserter(result));
+        return result;
+    }
+};
+
+class Test2 {
+public:
+    std::vector<std::string> commonChars(std::vector<std::string>& words) {
+        std::vector<std::string> result_str;
+        std::vector<char> result_char;
+
+        // convert the words to vector containing char
+        std::vector<std::vector<char>> vec_words(words.size());
+        for (auto i = 0; i < words.size(); i++){
+            for (auto j = 0; j < words[i].size(); j++){
+                vec_words[i].push_back(words[i][j]);
+            }
+        }
+        
+        std::set_intersection(vec_words[0].begin(), vec_words[0].end(),
+                              vec_words[1].begin(), vec_words[1].end(),
+                              std::back_inserter(result_char));
+
+        // change the char to string
+        for (char cha : result_char){
+            std::string char_str(1, cha);
+            result_str.push_back(char_str);
+        }
+        return result_str;
+    }
+};
+
+// not working now, because it cannot find the freq of a character
+class Solution2 {
+public:
+    std::vector<std::string> commonChars(std::vector<std::string>& words) {
+        std::vector<std::string> result_str;
+        std::vector<char> result_char(words[0].begin(), words[0].end());
+        std::vector<char> result_char_copy(words[0].begin(), words[0].end());
+
+        // vector has no member find
+        // vector erase element by index, cannot by value
+        // vector remove can delete all the elments with sam value
+    
+        for (std::string word : words){
+            for (auto i = 0; i < result_char.size(); i++){
+                // string find: https://www.cplusplus.com/reference/string/string/find/
+                // if we cannot find some cha in the word, remove it
+                std::cout << i << std::endl;
+                if (word.find(result_char[i]) == std::string::npos){
+                    std::cout << "delete: " << result_char[i] << std::endl; 
+                    result_char.erase(result_char.begin() + i);
+                }
+            }
+        }
+
+        // change the char to string
+        for (char cha : result_char){
+            std::string char_str(1, cha);
+            result_str.push_back(char_str);
+        }
+        return result_str;
+    }
+};
+
 int main(){
-    Solution solution;
+    Solution2 solution;
 
     std::vector<std::string> words1 = {"bella","label","roller"};
     info(solution.commonChars(words1));
@@ -62,4 +138,7 @@ int main(){
 
     std::vector<std::string> words3 = {"ab","abb","abba"};
     info(solution.commonChars(words3));
+
+    std::vector<std::string> words4 = {"abaa","abb","abba"};
+    info(solution.commonChars(words4));
 }
