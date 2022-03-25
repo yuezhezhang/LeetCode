c99312324326c13fe255a22ba0a0a7a4ca992155
yuezhezhang <zyz994141@gmail.com>
2022-03-25 18:26:02 +0100

    Update an answer to q 1002 python

--- python_ws/1002_find_common_characters.py
+++ python_ws/1002_find_common_characters.py
@@ -19,6 +19,21 @@ class Solution(object):
         
         return common_chars
 
+class Solution2(object):
+    def commonChars(self, A):
+        """
+        :type A: List[str]
+        :rtype: List[str]
+        """
+        res=[]
+        if not A:
+            return res
+        key=set(A[0])
+        for k in key:
+            minnum=min(a.count(k) for a in A)
+            res+=minnum*k
+        return res
+
 
 
 if __name__ == "__main__":
