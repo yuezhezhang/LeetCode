d13075a55618e8dd1177761fa385d92a2ea9f727
yuezhezhang <zyz994141@gmail.com>
2022-04-20 15:32:24 +0200

    Implement one solution to q202 cpp

--- cpp_ws/src/0202_happy_number.cpp
+++ cpp_ws/src/0202_happy_number.cpp
@@ -1,15 +1,43 @@
 #include "helper.cpp"
+#include <set>
+#include <cmath>
 
+// time: 4ms, beat 45.68%
+// RAM: 6.3MB, beat 6.83%
 class Solution {
 public:
-    bool isHappy(int n) {
+    int square_sum_digit(int n) {
+        int square_sum = 0;
+        while (n != 0){
+            square_sum += pow(n%10, 2); 
+            n = int (n/10);
+        }
+        return square_sum;
+    }
 
+    // the definition of square_sum_digit must be put above
+    bool isHappy(int n) {
+        // std::cout << square_sum_digit(n) << std::endl;
+        // std::cout << pow(n%10, 2) << std::endl;
+
+        std::set<int> appear_set = {n};
+        while (n != 1){
+            n = square_sum_digit(n);
+            if (appear_set.find(n) != appear_set.end())
+                return false;
+            else
+                appear_set.insert(n);
+        }
+        return true;
     }
 };
 
 int main(){
     Solution solution;
-    solution.isHappy(3);
+    
+    std::cout << solution.isHappy(1) <<std::endl;
+    std::cout << solution.isHappy(2) <<std::endl;
+    std::cout << solution.isHappy(19) <<std::endl;
 
     return 0;
 }
