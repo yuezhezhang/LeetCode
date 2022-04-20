0cbe23e87dc3f63cfe08ceaf59de7ad449d34e88
yuezhezhang <zyz994141@gmail.com>
2022-04-20 17:51:14 +0200

    Implement one solution to q454 python, but it is beyond time limit

--- python_ws/0454_4_sum_2.py
+++ python_ws/0454_4_sum_2.py
@@ -7,3 +7,30 @@ class Solution(object):
         :type nums4: List[int]
         :rtype: int
         """
+        n = len(nums1)
+        # Is there any duplicate in one list! yes!
+        num_tuple = 0
+        for i in set(nums1):
+            for j in set(nums2):
+                for k in set(nums3):
+                    # count the frequency in nums4
+                    # if it does not exist in the list, it returns 0
+                    num_tuple += nums1.count(i) * nums2.count(j) * nums3.count(k) * nums4.count(-(i + j + k))
+        return num_tuple
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    nums1 = [1, 2]
+    nums2 = [-2, -1]
+    nums3 = [-1, 2]
+    nums4 = [0, 2]
+    print(solution.fourSumCount(nums1, nums2, nums3, nums4))
+
+    nums1 = [0]
+    nums2 = [0]
+    nums3 = [0, 0]
+    nums4 = [0, 0]
+    print(solution.fourSumCount(nums1, nums2, nums3, nums4))
