76e46614a2709be36aba1944fd8e7a880844701f
yuezhezhang <zyz994141@gmail.com>
2022-04-21 16:05:29 +0200

    Implement one solution for q383 python

--- python_ws/0383_ransom_note.py
+++ python_ws/0383_ransom_note.py
@@ -1,3 +1,5 @@
+# time: 160ms, beat 13.39%
+# RAM: 12.8 MB, beat 99.92%
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
         """
@@ -5,3 +7,27 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
+        for i in ransomNote:
+            if ransomNote.count(i) > magazine.count(i):
+                return False
+
+        return True
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    ransomNote = "a"
+    magazine = "b"
+    print(solution.canConstruct(ransomNote, magazine))
+
+    ransomNote = "a"
+    magazine = "ba"
+    print(solution.canConstruct(ransomNote, magazine))
+
+    ransomNote = "ab"
+    magazine = "abbb"
+    print(solution.canConstruct(ransomNote, magazine))
+
+    ransomNote = "aab"
+    magazine = "baa"
+    print(solution.canConstruct(ransomNote, magazine))
