d23f066d4d18b9933aa81d8615795baff8aac95e
yuezhezhang <zyz994141@gmail.com>
2022-04-22 12:22:09 +0200

    Update README

--- python_ws/0018_4_sum.py
+++ python_ws/0018_4_sum.py
@@ -1,11 +1,3 @@
-'''
-Author: your name
-Date: 2022-04-22 12:08:04
-LastEditTime: 2022-04-22 12:17:30
-LastEditors: your name
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-FilePath: /LeetCode/python_ws/0018_4_sum.py
-'''
 class Solution(object):
     def fourSum(self, nums, target):
         """
