0b79bf0c9d75d6ace9160704407a467e015458ae
yuezhezhang <zyz994141@gmail.com>
2022-04-23 17:57:06 +0200

    Add one solution to q202 cpp

--- cpp_ws/src/0202_happy_number.cpp
+++ cpp_ws/src/0202_happy_number.cpp
@@ -32,8 +32,43 @@ public:
     }
 };
 
+// We can use fast iterator and slow iterator to find the cycle.
+// The fast iterator goes two steps every iteration and slow iterator goes one step 
+// every iteration, when they are equal, then it is a cycle. Now we need to determine 
+// if the cycle is yielded by 1, if so then it is a happy number, oherwise it is not.
+// It should be mentioned that it is not advisable to use set or recursion to represent 
+// the cycle, since the set might be too large to store.
+// time: 0ms, beat 100%
+// RAM: 5.8MB, beat 90.04%
+class Solution2 {
+public:
+    int square_sum_digit(int n) {
+        int square_sum = 0;
+        while (n != 0){
+            square_sum += pow(n%10, 2); 
+            n = int (n/10);
+        }
+        return square_sum;
+    }
+
+    // the definition of square_sum_digit must be put above
+    bool isHappy(int n) {
+        int slow = n, fast = n;
+        do{
+            slow = square_sum_digit(slow);
+            fast = square_sum_digit(fast);
+            fast = square_sum_digit(fast);
+            std::cout << "Slow iterator: " << slow << std::endl;
+            std::cout << "Fast iterator: " << fast << std::endl;
+        }while (slow != fast);
+
+        return slow == 1;
+    }
+};
+
+
 int main(){
-    Solution solution;
+    Solution2 solution;
     
     std::cout << solution.isHappy(1) <<std::endl;
     std::cout << solution.isHappy(2) <<std::endl;
