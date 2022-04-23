cdddab6c21c6bfcfdf5ddb55a14e132b0659a2b0
yuezhezhang <zyz994141@gmail.com>
2022-04-23 18:48:15 +0200

    Add two solutions to q383 python

--- python_ws/0383_ransom_note.py
+++ python_ws/0383_ransom_note.py
@@ -13,6 +13,46 @@ class Solution(object):
 
         return True
 
+# time: 56ms, beat 54.45%
+# RAM: 13.1MB, beat 72.77%
+class Solution2(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        dict = {}
+        for i in magazine:
+            dict[i] = dict.get(i, 0) + 1
+        for j in ransomNote:
+            dict[j] = dict.get(j, 0) - 1
+            if dict[j] < 0:
+                return False
+
+        return True
+# use array to store the frequency of character saves more space, since 
+# it will not need to store the key
+# time: 44ms, beat 79.43%
+# RAM: 13MB, beat 91.4%
+class Solution3(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        array = [0] * 26
+        # ord function to transform the char to ascii
+        for i in magazine:
+            array[ord(i) - ord('a')] += 1
+        for j in ransomNote:
+            array[ord(j) - ord('a')] -= 1
+            if array[ord(j) - ord('a')] < 0:
+                return False
+
+        return True
+
 if __name__ == "__main__":
     solution = Solution()
 
